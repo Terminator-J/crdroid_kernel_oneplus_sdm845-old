fb08c35bf4525c02bda628da033468e6760a5bc3
laxman <laxman.siripuram@oneplus.com>
2021-08-17 11:07:34 +0530

    drm: Synchronize codes for Oneplus 6T OxygenOS 11/ Oneplus 6 OxygenOS 11
    Change-Id: I85cc3b80e869140e627210167facaca3dac0b495

--- drivers/gpu/drm/msm/msm_drv.h
+++ drivers/gpu/drm/msm/msm_drv.h
@@ -99,6 +99,8 @@ enum msm_mdp_plane_property {
 
 	/* range properties */
 	PLANE_PROP_ZPOS = PLANE_PROP_BLOBCOUNT,
+	//sandeep.vulluri@OnePlus.MultiMediaService,2021/04/19, add for fingerprint
+	PLANE_PROP_CUSTOM,
 	PLANE_PROP_ALPHA,
 	PLANE_PROP_COLOR_FILL,
 	PLANE_PROP_H_DECIMATE,
@@ -161,6 +163,7 @@ enum msm_mdp_crtc_property {
 
 	CRTC_PROP_ENABLE_SUI_ENHANCEMENT,
 	CRTC_PROP_IDLE_PC_STATE,
+	CRTC_PROP_CUSTOM,
 
 	/* total # of properties */
 	CRTC_PROP_COUNT
@@ -195,6 +198,8 @@ enum msm_mdp_conn_property {
 	CONNECTOR_PROP_AUTOREFRESH,
 	CONNECTOR_PROP_LP,
 	CONNECTOR_PROP_FB_TRANSLATION_MODE,
+
+	CONNECTOR_PROP_CUSTOM,
 
 	/* total # of properties */
 	CONNECTOR_PROP_COUNT
@@ -628,6 +633,7 @@ struct msm_drm_private {
 
 	/* update the flag when msm driver receives shutdown notification */
 	bool shutdown_in_progress;
+	ktime_t commit_end_time;
 };
 
 /* get struct msm_kms * from drm_device * */
