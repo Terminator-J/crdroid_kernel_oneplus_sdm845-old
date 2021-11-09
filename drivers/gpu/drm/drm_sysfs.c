bcaef02ec8721c147048203e7fcc3942f26830b2
Art_Chen <Chenxy0201@qq.com>
2021-11-09 14:32:18 +0800

    drm: Implement panel status check
    * to ensure dimlayer has been killed when the panel is off.
    
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: I30606f5fb4fcd493a94b41792a75997c86ba4545

--- drivers/gpu/drm/drm_sysfs.c
+++ drivers/gpu/drm/drm_sysfs.c
@@ -848,6 +848,23 @@ static ssize_t native_display_srgb_color_mode_store(struct device *dev,
 	}
 	return count;
 }
+
+int oneplus_panel_status = 0;
+static ssize_t op_display_get_power_status(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", oneplus_panel_status);
+}
+
+static ssize_t op_display_set_power_status(struct device *dev,
+				struct device_attribute *attr,
+				const char *buf, size_t count)
+{
+	sscanf(buf, "%d", &oneplus_panel_status);
+
+	return count;
+}
+
 /******************************************************************/
 static DEVICE_ATTR_RW(status);
 static DEVICE_ATTR_RO(enabled);
@@ -887,6 +904,7 @@ static DEVICE_ATTR(dimlayer_bl_en, S_IRUGO|S_IWUSR,
 
 static DEVICE_ATTR_RW(dsi_panel_command);
 static DEVICE_ATTR_RW(dsi_seed_command);
+static DEVICE_ATTR(power_status, S_IRUGO | S_IWUSR, op_display_get_power_status, op_display_set_power_status);
 
 static struct attribute *connector_dev_attrs[] = {
 	&dev_attr_status.attr,
@@ -917,6 +935,7 @@ static struct attribute *connector_dev_attrs[] = {
 	&dev_attr_dimlayer_bl_en.attr,
 	&dev_attr_dp_en.attr,
 	&dev_attr_dither_en.attr,
+	&dev_attr_power_status.attr,
 	NULL
 };
 
